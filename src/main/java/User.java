5c9f4bb8f162f38d75f3e563b589aac37125e23a
Mahla <MahlaEntezari8926@gmail.com>
2023-03-08 23:07:42 +0330

    done

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class User {
     //User should have a list of books
     //User should have a username and a password
+    //Book should contain name,author,year of publish and ISBN
+    Scanner in = new Scanner(System.in);
+    private String UserName ;
+    private String Password;
+    private ArrayList<Book> Books;
+    public User(String userName,String password){
+        UserName = userName;
+        Password = password;
+        Books = new ArrayList<Book>();
+    }
 
-    public void rentBook(){
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return Books;
+    }
+    public void setBooks(ArrayList<Book> books) {
+        Books = books;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public void rentBook(Book book){
+        Books.add(book);
         //TODO
     }
 
-    public void returnBook(){
+    public void returnBook(Book book){
+        Books.remove(book);
         //TODO
     }
 }
